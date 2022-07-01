5374f739dc1ac660ae70a03f48923e2a58d1ed9f
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-07-01 12:04:10 +0700

    Bootcamp

--- week 1/ex/16_str_rev.py
+++ week 1/ex/16_str_rev.py
@@ -1,10 +1,10 @@
 str = input("Input a string:")
-def uprlow(str):
+def strrev(str):
     if str == "":
         print("The string is empty.")
     else:
-        s1 = str[0:len(str) // 2]
+        s1 = str[(len(str) // 2)-1::-1]
         s2 = str[len(str) // 2:]
-        print(s1.upper(), end ="")
-        print(s2.lower())
-uprlow(str)
+        print(s1, end="")
+        print(s2)
+strrev(str)
