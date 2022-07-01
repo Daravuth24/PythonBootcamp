439fa6e0a31fcae383280135dc434c923e28f820
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-07-01 11:33:37 +0700

    Bootcamp

--- week 1/ex/12_stringslice.py
+++ week 1/ex/12_stringslice.py
@@ -1,7 +1,10 @@
 str = input("Input a text:")
 def slicing(str):
-    s1 = str[0:len(str)//2]
-    s2 = str[len(str)//2:]
-    print(s1)
-    print(s2)
+    if str == "":
+        print("The string is empty.")
+    else:
+        s1 = str[0:len(str)//2]
+        s2 = str[len(str)//2:]
+        print(s1)
+        print(s2)
 slicing(str)
