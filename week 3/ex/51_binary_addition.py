41c4e8f9056f9642133cb7ea5648a51e9892a1ef
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-07-29 14:53:21 +0700

    Python Bootcamp

---
+++ week 3/ex/51_binary_addition.py
@@ -0,0 +1,22 @@
+def binary_addition(x, y):
+
+    binnumx = bin(x).replace("0b", "")
+
+    binnumy = bin(y).replace("0b", "")
+
+    sumbin = bin(x+y).replace("0b", "")
+
+    sumdec = x + y
+
+    print(f"binary_addtion({x},{y})")
+
+    print("Num 1 :", binnumx)
+
+    print("Num 2 :", binnumy)
+
+    print("Sum (Binary) :", sumbin)
+
+    print("Sum (Decimal) :", sumdec)
+
+
+binary_addition(60, 50)
