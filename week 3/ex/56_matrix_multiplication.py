1a8f2704d544a0b74b228f295c201087f88c5d40
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-08-01 00:31:16 +0700

    Python bootcamp

--- week 3/ex/56_matrix_multiplication.py
+++ week 3/ex/56_matrix_multiplication.py
@@ -7,13 +7,18 @@ def matrix_addition(x, y):
 
     m2 = matrix(y)
 
-    m3 = m1 * m2
+    if len(x) == len(y):
 
-    print(f"\nMatrix 1: \n \n{m1} \n")
+        m3 = m1 * m2
 
-    print(f"Matrix 2: \n \n {m2} \n")
+        print(f"\nMatrix 1: \n \n{m1} \n")
 
-    print(f"The result matrix is: \n \n{m3}")
+        print(f"Matrix 2: \n \n {m2} \n")
+
+        print(f"The result matrix is: \n \n{m3}")
+
+    else:
+        print("Error")
 
 
-matrix_addition('10 5 4 2; 5 0 9 5; 9 19 60 8; 7 8 4 5', '12 65 34 42; 10 5 89 45; 11 21 63 78; 87 78 54 65')
+matrix_addition('3 7 5; 2 6 7; 4 3 2', '6 5 4; 3 2 1; 1 2 3')
