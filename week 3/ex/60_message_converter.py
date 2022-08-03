d485bdf51ebfc905024c7b117b7f818a2fc62b06
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-08-03 23:08:43 +0700

    Python Bootcamp

--- week 3/ex/60_message_converter.py
+++ week 3/ex/60_message_converter.py
@@ -1,11 +1,19 @@
 def message_converter(mes):
+
     secret = []
+
     if mes == "":
         print("The string is empty.")
+
     else:
+
         for i in mes:
-            ascii = ord(i)
-            decnum = int(ascii)
+
+            asc = ord(i)
+            decnum = int(asc)
             secret = hex(decnum).upper().replace("0X", "")
+
             print(secret, end="")
+
+
 message_converter("Hello")
