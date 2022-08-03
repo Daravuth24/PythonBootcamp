167d4b6befbc01602f2540ce8ecd51ebc10876e6
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-08-03 16:48:41 +0700

    Python Bootcamp

--- week 3/ex/52_binary_subtraction.py
+++ week 3/ex/52_binary_subtraction.py
@@ -1,22 +1,49 @@
-def binary_substraction(x, y):
+def binary_subtraction(x, y):
 
-    binnumx = bin(x).replace("0b", "")
+    dec1 = x
+    dec2 = y
 
-    binnumy = bin(y).replace("0b", "")
+    bin1 = bin(int(x)).replace("0b", "")
+    bin2 = bin(int(y)).replace("0b", "")
 
-    substractbin = bin(x - y).replace("0b", "")
+    maxlen = max(len(bin1), len(bin2))
+    result = ''
+    carry = 0
 
-    substractdec = x - y
+    i = maxlen - 1
+    while i >= 0:
+
+        s = int(bin1[i]) - int(bin2[i])
+
+        if s == -1:
+            if carry == 0:
+                carry = 1
+                result = result + "1"
+
+        if s == 0:
+            if carry == 0:
+                result = result + "0"
+
+        if s == 1:
+            if carry == 1:
+                result = result + "0"
+                carry = 0
+            else:
+                result = result + "1"
+
+        i = i - 1
+
+    if carry > 0:
+        result = result + "1"
+
+    difbin = int(result[::-1])
+    difdec = int(dec1) - int(dec2)
 
     print(f"binary_addtion({x},{y})")
-
-    print("Num 1 :", binnumx)
-
-    print("Num 2 :", binnumy)
-
-    print("Sum (Binary) :", substractbin)
-
-    print("Sum (Decimal) :", substractdec)
+    print("Num 1 :", bin1)
+    print("Num 2 :", bin2)
+    print("Difference (Binary) :", difbin)
+    print("Difference (Decimal) :", difdec)
 
 
-binary_substraction(60, 50)
+binary_subtraction('60', '50')
