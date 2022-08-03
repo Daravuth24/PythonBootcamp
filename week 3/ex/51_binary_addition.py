0d48facd2934f702202fedc379a095561d553a10
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-08-03 09:51:44 +0700

    Python Bootcamp

--- week 3/ex/51_binary_addition.py
+++ week 3/ex/51_binary_addition.py
@@ -1,18 +1,40 @@
 def binary_addition(x, y):
 
-    binnumx = bin(x).replace("0b", "")
+    binnum1 = bin(int(x)).replace("0b", "")
+    binnum2 = bin(int(y)).replace("0b", "")
 
-    binnumy = bin(y).replace("0b", "")
+    decnum1 = str(x)
+    decnum2 = str(y)
 
-    sumbin = bin(x + y).replace("0b", "")
+    sumbin = []
+    carry = "0"
 
-    sumdec = x + y
+    for i in range(len(binnum1)-1, -1, -1):
+        bin1 = binnum1[i]
+        bin2 = binnum2[i]
+        if bin1 == "0" and bin2 == "0" and carry == "0":
+            sumbin.append("0")
+            carry = "0"
+        elif (bin1 == "0" and bin2 == "0" and carry == "1") or (bin1 == "0" and bin2 == "1" and carry == "0") or (bin1 == "1" and bin2 == "0" and carry == "0"):
+            sumbin.append("1")
+            carry = "0"
+        elif bin1 == "1" and bin2 == "1" and carry == "0":
+            sumbin.append("0")
+            carry = "1"
+        elif bin1 == "1" and bin2 == "1" and carry == "1":
+            sumbin.append("1")
+            carry = "1"
+    if carry == "1":
+        sumbin.append("1")
+
+    sumbin = "".join(sumbin[::-1])
+    sumdec = int(sumbin, 2)
 
     print(f"binary_addtion({x},{y})")
 
-    print("Num 1 :", binnumx)
+    print("Num 1 :", binnum1)
 
-    print("Num 2 :", binnumy)
+    print("Num 2 :", binnum2)
 
     print("Sum (Binary) :", sumbin)
 
