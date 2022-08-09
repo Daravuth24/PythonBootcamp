87a478d732e5d68f110b7fb1409903305fed7a43
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2022-08-10 05:56:45 +0700

    Python Bootcamp

--- week 4/ex/68_json_to_tsv.py
+++ week 4/ex/68_json_to_tsv.py
@@ -1,5 +1,24 @@
 import json
 import csv
 
-def json_to_tsv(tsvname, jsonname):
 
+def json_to_tsv():
+
+    json_file = open("datatest.json", "r")
+    json_data = json.load(json_file)
+    json_file.close()
+
+    data = json.dumps(json_data)
+
+    tsv_file = open("data.tsv", "w")
+    tsv_writer = csv.writer(tsv_file, delimiter='\t')
+
+    tsv_writer.writerow(data[0].keys())
+
+    for row in data:
+        tsv_writer.writerow(row.values())
+
+    tsv_file.close()
+
+
+json_to_tsv()
